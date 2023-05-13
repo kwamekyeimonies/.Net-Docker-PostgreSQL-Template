e8a5d544efb2e3f455be203a4e2ac21ee3c27736
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-13 04:44:37 +0000

    bugs fixed

--- Controllers/DriversControllers.cs
+++ Controllers/DriversControllers.cs
@@ -13,22 +13,23 @@ namespace Demo.Microservices.Infrastructure.Controllers
         private readonly ILogger<DriversController>? _logger;
         private readonly ApiDbContext _context;
 
-        public DriversController(ILogger<DriversController> logger,ApiDbContext context)
+        public DriversController(ILogger<DriversController> logger, ApiDbContext context)
         {
             _logger = logger;
             _context = context;
         }
 
         [HttpGet("drivers")]
-        public async Task<IActionResult> GetAllDriversByAsync(){
-            var driver = new Driver()
-            {
-                DriverNuber = 66,
-                Name = "Prof.Tenkorang Daniel"
-            };
+        public async Task<IActionResult> GetAllDriversByAsync()
+        {
+            // var driver = new Driver()
+            // {
+            //     DriverNuber = 66,
+            //     Name = "Prof.Tenkorang Daniel"
+            // };
 
-            _context.Add(driver);
-            await _context.SaveChangesAsync();
+            // _context.Add(driver);
+            // await _context.SaveChangesAsync();
 
             var allDrivers = await _context.Drivers.ToListAsync();
 
